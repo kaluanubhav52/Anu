7949038485fb440b4ee52cc28285b7b814aa5832
kaluanubhav52 <kaluanubhav52@gmail.com>
2026-04-03 23:01:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -91,21 +91,20 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('ᴀʙᴏᴜᴛ 📜', callback_data='about'),
-            InlineKeyboardButton('ᴜᴘɢʀᴀᴅᴇ 🎟', callback_data="premium_info")
-        ],[
-            InlineKeyboardButton('🌸 ᴀɴɪᴍᴇ ɢᴜɪᴅᴇ 🌸', user_id=int(OWNER))
-        ]]
-        
+                    InlineKeyboardButton('ᴀʙᴏᴜᴛ 📜', callback_data='about'),
+                    InlineKeyboardButton('ᴜᴘɢʀᴀᴅᴇ 🎟', callback_data="premium_info")
+                ],[
+                    InlineKeyboardButton('🌸 ᴀɴɪᴍᴇ ɢᴜɪᴅᴇ 🌸', user_id=int(OWNER))
+                ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        m = await message.reply_sticker("CAACAgUAAxkBAAI47WjK1V24t_kyUL-ywJQQdxtaWnaeAAIMFgACpzEZVdpZS0jMvfn5HgQ")
+        m=await message.reply_sticker("CAACAgUAAxkBAAI47WjK1V24t_kyUL-ywJQQdxtaWnaeAAIMFgACpzEZVdpZS0jMvfn5HgQ")
         await asyncio.sleep(2)
         await m.delete()        
-        await message.reply_text(
-            text=script.START_TXT.format(message.from_user.mention, get_status(), temp.U_NAME, temp.B_NAME),
+        await message.reply_photo(
+            photo=random.choice(PICS),
+            caption=script.START_TXT.format(message.from_user.mention, get_status(), temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML,
-            disable_web_page_preview=True  
+            parse_mode=enums.ParseMode.HTML
         )
         return
     if message.command[1].startswith("reff_"):
